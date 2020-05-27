39b28f772d368bcfdd891d9c9bb562657dba65ea
Kunal Dutta <kdutta@berkeley.edu>
2020-05-27 11:59:47 -0700

    added convert method to send request to calculate base change

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from src.convert import changeBase
 
 app = Flask(__name__)
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route('/convert', methods=['GET', 'POST'])
+def convert():
+    # do something
+    pass
 
 
 if __name__ == "__main__":
