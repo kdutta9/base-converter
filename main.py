51ad06ce38eb4a1df7439be1db0ecdb30f2e3e89
Kunal Dutta <kdutta@berkeley.edu>
2020-05-28 21:45:55 -0700

    used dynamic variables to display result on main webpage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, jsonify
 from src.convert import changeBase
 
 app = Flask(__name__)
@@ -6,18 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", num="", fromBase="", toBase="", res="")
 
 
-@app.route("/convert", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def convert():
     if request.method == "POST":
         num = request.form["num"]
         fromBase = int(request.form["from"])
         toBase = int(request.form["to"])
-        return jsonify(changeBase(num, fromBase, toBase))
+        res = changeBase(num, fromBase, toBase)
+        print("Result = " + res)
+        return render_template("index.html", num=num, fromBase=fromBase, toBase=toBase, res=res)
     else:
-        return render_template("index.html")
+        return render_template("index.html", num="", fromBase="", toBase="", res="")
 
 
 if __name__ == "__main__":
