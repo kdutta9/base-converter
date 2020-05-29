3f07e45a3b6ab06a63705dc3b0f253f8cd8d92b6
Kunal Dutta <kdutta@berkeley.edu>
2020-05-28 20:06:33 -0700

    added requests from HTML form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect, jsonify
 from src.convert import changeBase
 
 app = Flask(__name__)
@@ -9,10 +9,15 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/convert', methods=['GET', 'POST'])
+@app.route("/convert", methods=['GET', 'POST'])
 def convert():
-    # do something
-    pass
+    if request.method == "POST":
+        num = request.form["num"]
+        fromBase = int(request.form["from"])
+        toBase = int(request.form["to"])
+        return jsonify(changeBase(num, fromBase, toBase))
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
